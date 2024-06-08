3f2f7f5f940eb71dfb54d3ebcd7f495e6c1fba22
daywalkers <k3yt07@gmail.com>
2024-06-08 22:03:01 +0700

    Update mailList.js

--- api/src/api/mailList.js
+++ api/src/api/mailList.js
@@ -2,8 +2,21 @@
 const mailgunReader = require("../mailgunReader");
 const mailgunConfig = require("../../config/mailgunConfig");
 const cacheControl  = require("../../config/cacheControl");
-
 const reader = new mailgunReader(mailgunConfig);
+
+// Function to validate and sanitize the recipient username
+function validateAndSanitizeRecipient(recipient) {
+  // Remove leading/trailing whitespace
+  recipient = recipient.trim();
+
+  // Validate that the recipient contains only allowed characters
+  const allowedCharacters = /^[a-zA-Z0-9._-]+$/;
+  if (!allowedCharacters.test(recipient)) {
+    throw new Error("Invalid recipient username");
+  }
+
+  return recipient;
+}
 
 /**
  * Mail listing api, returns the list of emails
@@ -11,27 +24,35 @@ const reader = new mailgunReader(mailgunConfig);
  * @param {*} req
  * @param {*} res
  */
-module.exports = function(req, res){
+module.exports = function(req, res) {
+  let params = req.query;
+  let recipient = params.recipient;
 
-	let params = req.query
-	let recipient = params.recipient
+  if (recipient == null) {
+    res.status(400).send({ error: "No `recipient` param found" });
+    return;
+  }
 
-	if (recipient == null){
-		res.status(400).send({ error : "No `recepient` param found" });
-	}
+  try {
+    // Validate and sanitize the recipient username
+    recipient = validateAndSanitizeRecipient(recipient);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+    return;
+  }
 
-	// strip off all @domain if there is any
-	if(recipient.indexOf("@") >= 0){
-		recipient = recipient.substring(0, recipient.indexOf("@"))
-	}
+  // Strip off all @domain if there is any
+  if (recipient.indexOf("@") >= 0) {
+    recipient = recipient.substring(0, recipient.indexOf("@"));
+  }
 
-	reader.recipientEventList(recipient+"@"+mailgunConfig.emailDomain)
-		.then(response => {
-			res.set('cache-control', cacheControl.dynamic)
-			res.status(200).send(response.items)
-		})
-		.catch(e => {
-			console.error(`Error getting list of messages for "${recipient}":`, e);
-			res.status(500).send("{error: '"+e+"'}")
-		});
-}
+  reader.recipientEventList(recipient + "@" + mailgunConfig.emailDomain)
+    .then(response => {
+      res.set('cache-control', cacheControl.dynamic);
+      res.status(200).send(response.items);
+    })
+    .catch(e => {
+      console.error(`Error getting list of messages for "${recipient}":`, e);
+      res.status(500).send({ error: 'Internal Server Error' });
+    });
+};
