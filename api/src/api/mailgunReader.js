94c55740a67aaff276ed13da9a525ea80dbbbaeb
daywalkers <k3yt07@gmail.com>
2024-06-13 05:48:16 +0700

    Create mailgunReader.js

---
+++ api/src/api/mailgunReader.js
@@ -0,0 +1,24 @@
+// api/src/mailgunReader.js
+
+class MailgunReader {
+    constructor(config) {
+        this.config = config;
+        this.mailgun = require('mailgun-js')({apiKey: config.apiKey, domain: config.emailDomain});
+    }
+
+    // Method to list all emails
+    listAllEmails() {
+        return new Promise((resolve, reject) => {
+            this.mailgun.events().list({event: 'accepted'}, function (error, body) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(body);
+                }
+            });
+        });
+    }
+
+}
+
+module.exports = MailgunReader;
