d7657f61ab7e42fafd7ec70c9e57f9baa4f2715f
daywalkers <k3yt07@gmail.com>
2024-07-15 07:14:12 +0700

    Update mailGetUrl.js

--- api/src/api/mailGetUrl.js
+++ api/src/api/mailGetUrl.js
@@ -1,52 +1,31 @@
 // Loading mailgun reader and config
 const mailgunReader = require("../mailgunReader");
 const mailgunConfig = require("../../config/mailgunConfig");
-const cacheControl = require("../../config/cacheControl");
+const cacheControl  = require("../../config/cacheControl");
 
 const reader = new mailgunReader(mailgunConfig);
 
 /**
- * Validate the URL parameter
- *
- * @param {String} url
- */
-function validateUrl(url) {
-  // Remove leading/trailing whitespace
-  url = url.trim();
-
-  // Ensure the URL is not empty
-  if (url === '') {
-    throw new Error("Invalid URL");
-  }
-
-  return url;
-}
-
-/**
- * Get and return the URL link from the mailgun API - for the mail content
+ * Get and return the URL link from the mailgun API - for the mail gcontent
+ * 
+ * NOTE - this is to be deprecated
  *
  * @param {*} req
  * @param {*} res
  */
-module.exports = function (req, res) {
-  let url = req.query.url;
+module.exports = function(req, res){
+	let params = req.query
+	let url = params.url
+	if (url == null || url === ""){
+		 res.status(400).send('{ "error" : "No `url` param found" }');
+	}
 
-  if (url == null || url === "") {
-    return res.status(400).send('{ "error" : "No `url` param found" }');
-  }
-
-  try {
-    url = validateUrl(url);
-  } catch (error) {
-    return res.status(400).send({ error: error.message });
-  }
-
-  reader.getUrl(url).then(response => {
-    res.set('cache-control', cacheControl.static);
-    res.status(200).send(response);
-  })
-  .catch(e => {
-    console.error("Error: ", e);
-    res.status(500).send({ error: 'Internal Server Error' });
-  });
-};
+	reader.getUrl(url).then(response => {
+		res.set('cache-control', cacheControl.static)
+		res.status(200).send(response)
+	})
+	.catch(e => {
+		console.error("Error: ", error)
+		res.status(500).send("{error: '"+e+"'}")
+	});
+}
